2485d5242c2dfffce941ccb557efabe04b36d5a1
Max <mpbakunov@gmail.com>
2024-10-07 13:22:28 +0200

    Solution (Spring Boot Security) Bugs fixes #02.

--- src/main/java/com/example/onlinebookstore/security/CustomUserDetailsService.java
+++ src/main/java/com/example/onlinebookstore/security/CustomUserDetailsService.java
@@ -16,6 +16,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException("Can't find by user name"));
+                .orElseThrow(() -> new EntityNotFoundException("Can't find by email:" + email));
     }
 }
